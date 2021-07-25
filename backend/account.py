e05cf694013587a5d2b30792fbd47db5918eecd5
Arthur Laurent <laurent.arthur75@gmail.com>
2021-07-25 23:02:08 +0200

    Simplify account code, convert to dataclass, and remove useless code

--- backend/account.py
+++ backend/account.py
@@ -1,11 +1,13 @@
+from dataclasses import dataclass, field, replace
+from typing import Dict
+
 from backend.asset import Asset
 from backend.binance import binance
 
 
+@dataclass
 class Account:
-    def __init__(self):
-        self.assets = {}
-        self.load()
+    assets: Dict[str, Asset] = field(default_factory=dict)
 
     def load(self):
         account_info = binance.get_account_info()
@@ -22,18 +24,8 @@ class Account:
         for asset in self.assets.values():
             converted = asset.to(asset_name)
             if converted:
-                base_asset += converted
+                base_asset = replace(base_asset, amount=base_asset.amount + converted.amount)
             else:
-                print("NOT FOUND", asset, "to", asset_name)
+                raise Exception("Couldn't find a way to convert {asset.name} to {asset_name}")
 
         return base_asset
-
-    def __repr__(self):
-        strings = ["<Account"]
-
-        for asset in sorted(self.assets.values(), key=lambda c: c.amount, reverse=True):
-            strings.append(f"  {asset}")
-
-        strings.append(">")
-
-        return "\n".join(strings)
